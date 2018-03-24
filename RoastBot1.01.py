5910abcbce9f5dc1e3a292dd2bc0262c185c69ad
FranklyPython <37746980+FranklyPython@users.noreply.github.com>
2018-03-24 17:04:25 +0000

    Added RoastBot1.01
    nothing much really. just adding RB1.01

---
+++ RoastBot1.01.py
@@ -0,0 +1,39 @@
+import sys
+import random
+
+ans = "true"
+
+while ans:
+    question = input("Roast the RoastBot1.01. It replies, so give it hell! (Press the 0 key to exit the RoastBot1.01)")
+
+    answers = random.randint(1,9)
+
+    if question == 0:
+        sys.exit()
+
+    elif answers ==1:
+        print("Quit moving your head about. you'll hit a fucking plane with your somalian harline.")
+
+    elif answers ==2:
+        print ("Fam, brush your fucking teeth. they remind me of some song... How did it go again? Oh.. That's right! Black and yellow, black and yellow!")
+
+    elif answers ==3:
+        print ("you so fucking ugly you put my nigga Chucky Cheese out of business. might as well make a new restaurant 'Ugly Nigga's Pizza!'")
+
+    elif answers ==4:
+        print ("Do i have to roast you anymore? you basically roasted yourself by being born. I mean, your teeth are fucking outta whack, and you a fat piece of shit!")
+
+    elif answers ==5:
+        print ("Nullified Reverse Card. No U")
+
+    elif answers ==6:
+        print ("Your teeth look like cheesy Doritos. wat did you eat this morning?")
+
+    elif answers ==7:
+        print ("Nigga you as smart as a fucking fox. you rape and think you can get away with it!")
+
+    elif answers ==8:
+        print ("your lips are so dry that you have The Krusty Krab on those desert like chops. Is their food great, btw?")
+
+    elif answers ==9:
+        print ("You be like the last and first slices of bread. everyone touches you but nobody wants you.")
